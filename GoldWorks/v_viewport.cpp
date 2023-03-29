81ad4c7fa7b95cf06fc6428ebb8857c239b6be88
GoldenStarGamer <vicentetavareslima@gmail.com>
2023-03-29 22:26:16 +0100

    yuyuh7uynvt4quyhj

--- GoldWorks/v_viewport.cpp
+++ GoldWorks/v_viewport.cpp
@@ -1,5 +1,9 @@
 #include "v_viewport.hpp"
 
+// 3:30
+
 namespace gwe {
-	gwViewport::gwViewport(int w=, int h, std::string name ) : width{w}, height{h}, viewportName{name}
+	gwViewport::gwViewport(int w, int h, std::string name) : width{ w }, height{ h }, viewportName{ name } {
+
+	}
 }
